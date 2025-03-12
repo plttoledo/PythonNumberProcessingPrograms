c2409fd48a02c19aa7afaedd788bd4ef6ab981e3
plttoledo <toledopreciouslaine@gmail.com>
2025-03-12 18:01:56 +0800

    Updated, GetNumberSum.py file, two inputs added to get the sum result.

--- Batch1/GetNumberSum.py
+++ Batch1/GetNumberSum.py
@@ -1,2 +1,8 @@
 # Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
 # User input 2 numbers, print the sum of the two numbers.
+
+user_input1 = float(input("Input the first number: "))
+user_input2 = float(input("Input the second number: "))
+
+user_sum = user_input1 + user_input2
+print(f"{user_input1} + {user_input2} results to {user_sum}.")
