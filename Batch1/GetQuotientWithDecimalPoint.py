84a71c6c86a04689d6c2cf2260e0152fa18dec8a
plttoledo <toledopreciouslaine@gmail.com>
2025-03-12 18:15:15 +0800

    Added the actual code. Basic division operation work as intended.

--- Batch1/GetQuotientWithDecimalPoint.py
+++ Batch1/GetQuotientWithDecimalPoint.py
@@ -1,3 +1,9 @@
 # Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the
 # decimal point.
 # User input 2 numbers, print the quotient of the two numbers with a decimal point.
+
+user_input1 = float(input("Input the first number: "))
+user_input2 = float(input("Input the second number: "))
+
+user_quotient = user_input1 / user_input2
+print(f"{user_input1} divide by {user_input2} results to {user_quotient}.")
