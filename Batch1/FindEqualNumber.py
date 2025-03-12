3623f6b32fa738a74c8b45b847f59c8127363f4e
plttoledo <toledopreciouslaine@gmail.com>
2025-03-12 17:57:27 +0800

    Updated FindEqualNumber.py, program outputs equal or not equal.

--- Batch1/FindEqualNumber.py
+++ Batch1/FindEqualNumber.py
@@ -1,2 +1,10 @@
 # Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
 # User input 2 numbers, printt equal when the numbers are same.
+
+user_input1 = float(input("Input the first number: "))
+user_input2 = float(input("Input the second number: "))
+
+if user_input1 == user_input2:
+    print(f"The two numbers are equal.")
+else:
+    print(f"The two numbers are not equal.")
