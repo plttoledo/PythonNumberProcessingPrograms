4af13c20c786b723372a426aa9f7c6ed54f34d7c
plttoledo <toledopreciouslaine@gmail.com>
2025-03-12 18:46:35 +0800

    Updated the program. Added the loop that excludes numbers with 0.

--- Batch1/LoopPrintNumbersExceptWithZero.py
+++ Batch1/LoopPrintNumbersExceptWithZero.py
@@ -1,2 +1,6 @@
 # Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
 # Loop program that prints all numbers from 0 to 100 except numbers that ends in zero.
+
+for num in range(101):
+    if num % 10 != 0:
+        print(num)
