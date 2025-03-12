822581f2fa53e8dfcff107f371affacc4621be83
plttoledo <toledopreciouslaine@gmail.com>
2025-03-12 18:42:58 +0800

    Updated the program. Added the even number loop code from 0 to 100.

--- Batch1/PrintEvenNumbersToOneHundredLoop.py
+++ Batch1/PrintEvenNumbersToOneHundredLoop.py
@@ -1,2 +1,5 @@
 # Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
 # Loop program that prints all even numbers from 0 to 100.
+
+for num in range(0,101,2):
+    print(num)
