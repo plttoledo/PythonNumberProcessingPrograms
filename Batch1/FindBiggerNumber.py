5f74e7b29b331cb75b2984053808b8f51c83c0b8
plttoledo <toledopreciouslaine@gmail.com>
2025-03-12 17:46:04 +0800

    Updated FindBiggerNumber.py

--- Batch1/FindBiggerNumber.py
+++ Batch1/FindBiggerNumber.py
@@ -1,2 +1,10 @@
 # Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
 # User input 2 numbers, print the bigger number.
+
+user_input1 = float(input("Input the first number: "))
+user_input2 = float(input("Input the second number: "))
+
+if user_input1 > user_input2:
+    print(f"{user_input1} is the bigger number")
+else:
+    print(f"{user_input2} is the bigger number")
