5a87df166907af43698d35e5d2005e30c8ed396a
plttoledo <toledopreciouslaine@gmail.com>
2025-03-12 18:38:15 +0800

    Added the code, able to count the odd numbers.

--- Batch1/GetTenCountOddNumbers.py
+++ Batch1/GetTenCountOddNumbers.py
@@ -1,2 +1,10 @@
 # Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
 # User input 10 numbers, print how many odd numbers.
+
+odd_num = 0
+for i in range(10):
+    num = float(input(f"Input #{i+1}: "))
+    if num % 2 != 0:
+        odd_num += 1
+
+print(f"There are {odd_num} odd numbers.")
