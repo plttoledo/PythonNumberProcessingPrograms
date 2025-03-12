b2703e7734d235bf356e6e74eb992682b7bfe16f
plttoledo <toledopreciouslaine@gmail.com>
2025-03-12 18:31:06 +0800

    Updated the program, sum of 10 numbers (including negatives) work.

--- Batch1/GetSumTenNumbers.py
+++ Batch1/GetSumTenNumbers.py
@@ -1,2 +1,8 @@
 # Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
 # User input 10 numbers, print the sum of all the numbers.
+total = 0
+for i in range(10):
+    num = float(input(f"Input #{i+1}: "))
+    total += num
+
+print(f"The sum of the 10 numbers is {total}.")
