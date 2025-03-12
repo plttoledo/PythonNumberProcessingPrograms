1c338b4085e9a47d3ffc3df06ec8db710274c5bc
plttoledo <toledopreciouslaine@gmail.com>
2025-03-12 18:08:28 +0800

    Added the actual code into the program, get result times by the two inputs.

--- Batch1/GetProductOfNumbers.py
+++ Batch1/GetProductOfNumbers.py
@@ -1,2 +1,8 @@
 # Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
 # User input 2 numbers, print the product of the two numbers.
+
+user_input1 = float(input("Input the first number: "))
+user_input2 = float(input("Input the second number: "))
+
+user_product = user_input1 * user_input2
+print(f"{user_input1} times {user_input2} results to {user_product}.")
