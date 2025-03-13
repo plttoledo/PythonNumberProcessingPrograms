f8a2fab7b242ee1388df77c5ce58175d83f6d8a9
plttoledo <toledopreciouslaine@gmail.com>
2025-03-14 02:47:00 +0800

    Added new folder and updated some files

---
+++ batch_four/prog_one.py
@@ -0,0 +1,14 @@
+# Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+
+
+numbers = []
+entries = []
+
+for i in range(10):
+    num = int(input(f"Enter #{i+1}: "))
+    numbers.append(num)
+
+for num in numbers:
+    if numbers.count(num) != 1 and num not in entries:
+        print(num)
+        entries.append(num)
