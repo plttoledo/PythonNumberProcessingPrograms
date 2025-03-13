60bb958775f6aaeabb9edc46f53b705b48e6ed1a
plttoledo <toledopreciouslaine@gmail.com>
2025-03-13 21:35:30 +0800

    Added pseudocode, the actual code for it, as well as renamed the file.

--- Batch2/for_loop_print_except_numbers_ending_zero_or_five.py
+++ Batch2/for_loop_print_except_numbers_ending_zero_or_five.py
@@ -1,3 +1,7 @@
 # Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
 # Loop print all numbers from 0 to 100 except numbers ending in 0 or 5
 
+for num in range(101):
+    if num % 10 != 0 and num % 5 != 0:
+        print(num)
+
